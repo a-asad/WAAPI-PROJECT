15ed2a899ba58710afde8d733172d6e5d48adc1f
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-07-17 11:33:56 +0500

    //all done.. //speeds up on click.. //slows down automatically to normal speed.. //working fine

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         {transform: 'rotate(360deg)'},
     ],
     timing: {
-      duration: 10000,
+      duration: 60000,
       iterations: Infinity,
       delay:2,
       easing: "linear",
@@ -23,7 +23,7 @@ function App() {
       {backgroundPosition:'50% 50%'},
   ],
   timing: {
-    duration: 10000,
+    duration: 60000,
     iterations: Infinity,
     easing: "linear",
   }
@@ -34,7 +34,7 @@ function App() {
       {transform: 'translate(-2400px)'},
   ],
   timing: {
-    duration: 6000,
+    duration: 12000,
     iterations: Infinity,
     easing: "linear",
   }
@@ -70,7 +70,7 @@ function App() {
       {left:'40%'},
   ],
   timing: {
-    duration: 10000,
+    duration: 30000,
     iterations: Infinity,
     direction:"alternate",
     easing: "ease-in-out",
@@ -83,60 +83,81 @@ function App() {
       {transform: 'translate(-2400px)'},
   ],
   timing: {
-    duration: 10000,
+    duration: 15000,
     iterations: Infinity,
     easing: "linear",
   }
   });
   
-  useEffect(()=>{
+  function speedUp(){
+    buildingAnim.getAnimation().updatePlaybackRate(buildingAnim.getAnimation().playbackRate*1.15);
+    moonAnim.getAnimation().updatePlaybackRate(moonAnim.getAnimation().playbackRate*1.15);
+    bgAnim.getAnimation().updatePlaybackRate(bgAnim.getAnimation().playbackRate*1.15);
+    roadAnim.getAnimation().updatePlaybackRate(roadAnim.getAnimation().playbackRate*1.15);
+    wheel1Anim.getAnimation().updatePlaybackRate(wheel1Anim.getAnimation().playbackRate*1.15);
+    wheel2Anim.getAnimation().updatePlaybackRate(wheel2Anim.getAnimation().playbackRate*1.15);
+    carAnim.getAnimation().updatePlaybackRate(carAnim.getAnimation().playbackRate*1.15);
+  }
 
+  useEffect(()=>{
+    setInterval(()=>{
+      if(bgAnim.getAnimation().playbackRate > 1)
+      {
+        buildingAnim.getAnimation().updatePlaybackRate(buildingAnim.getAnimation().playbackRate*0.9);
+        moonAnim.getAnimation().updatePlaybackRate(moonAnim.getAnimation().playbackRate*0.9);
+        bgAnim.getAnimation().updatePlaybackRate(bgAnim.getAnimation().playbackRate*0.9);
+        roadAnim.getAnimation().updatePlaybackRate(roadAnim.getAnimation().playbackRate*0.9);
+        wheel1Anim.getAnimation().updatePlaybackRate(wheel1Anim.getAnimation().playbackRate*0.9);
+        wheel2Anim.getAnimation().updatePlaybackRate(wheel2Anim.getAnimation().playbackRate*0.9);
+        carAnim.getAnimation().updatePlaybackRate(carAnim.getAnimation().playbackRate*0.9);
+      }
+    },2000)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   },[])
 
   return (
-    
-    <div id="mainBg" ref={bgAnim.ref}>
-          <div class="moon" ref={moonAnim.ref}></div>
+    <div id="mainBg" ref={bgAnim.ref} onClick={speedUp}>
+          <div className="moon" ref={moonAnim.ref}></div>
 
-    <div class="skyline" ref={buildingAnim.ref}>
-        <div class="building1-shadow"></div>
-        <div class="building1">
-            <div class="building-left-half"></div>
-            <div class="building-right-half"></div>
+    <div className="skyline" ref={buildingAnim.ref}>
+        <div className="building1-shadow"></div>
+        <div className="building1">
+            <div className="building-left-half"></div>
+            <div className="building-right-half"></div>
         </div>
   </div>
-    <div class="road">
-        <div class="road-top-half" ref={roadAnim.ref}></div>
-        <div class="road-bottom-half"></div>
+    <div className="road">
+        <div className="road-top-half" ref={roadAnim.ref}></div>
+        <div className="road-bottom-half"></div>
     </div>
 
-      <div class="car-container" ref={carAnim.ref}>
-          <div class="car-top1">
-              <div class="window1"></div>
-              <div class="window2"></div>
+      <div className="car-container" ref={carAnim.ref}>
+          <div className="car-top1">
+              <div className="window1"></div>
+              <div className="window2"></div>
 
           </div>
-          <div class="car-top2">
-              <div class="door">
-                  <div class="door-knob"></div>
+          <div className="car-top2">
+              <div className="door">
+                  <div className="door-knob"></div>
               </div>
           </div>
-          <div class="car-bottom">
-              <div class="wheel1-top"></div>
-              <div class="wheel1" ref={wheel1Anim.ref}>
-                  <div class="wheel-dot1"></div>
-                  <div class="wheel-dot2"></div>
-                  <div class="wheel-dot3"></div>
-                  <div class="wheel-dot4"></div>
+          <div className="car-bottom">
+              <div className="wheel1-top"></div>
+              <div className="wheel1" ref={wheel1Anim.ref}>
+                  <div className="wheel-dot1"></div>
+                  <div className="wheel-dot2"></div>
+                  <div className="wheel-dot3"></div>
+                  <div className="wheel-dot4"></div>
 
               </div>
 
-              <div class="wheel2-top"></div>
-              <div class="wheel2" ref={wheel2Anim.ref}>
-                  <div class="wheel-dot1"></div>
-                  <div class="wheel-dot2"></div>
-                  <div class="wheel-dot3"></div>
-                  <div class="wheel-dot4"></div>
+              <div className="wheel2-top"></div>
+              <div className="wheel2" ref={wheel2Anim.ref}>
+                  <div className="wheel-dot1"></div>
+                  <div className="wheel-dot2"></div>
+                  <div className="wheel-dot3"></div>
+                  <div className="wheel-dot4"></div>
               </div>
           </div>
       </div>    
